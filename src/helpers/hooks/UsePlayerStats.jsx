82259e5a9e7f8ea509aea2b5e4f18570c04c2a84
Miguel Pinzon <cmpinzonh@unal.edu.co>
2023-12-22 15:47:44 -0500

    Modified hook to stop upgrade at 99

--- src/helpers/hooks/UsePlayerStats.jsx
+++ src/helpers/hooks/UsePlayerStats.jsx
@@ -4,16 +4,18 @@ const usePlayerStats = (initialStats) => {
   const [stats, setStats] = useState(initialStats);
 
   const increaseStats = () => {
-    setStats({
-      ...stats,
-      rating: stats.rating + 2,
-      pace: stats.pace + 2,
-      shooting: stats.shooting + 2,
-      passing: stats.passing + 2,
-      dribbling: stats.dribbling + 2,
-      defending: stats.defending + 2,
-      physicality: stats.physicality + 2,
-    });
+    if (stats.rating <= 97 && stats.pace <= 97 && stats.shooting <= 97 && stats.passing <= 97 && stats.dribbling <= 97 && stats.defending <= 97 && stats.physicality <= 97) {
+      setStats({
+        ...stats,
+        rating: stats.rating + 2,
+        pace: stats.pace + 2,
+        shooting: stats.shooting + 2,
+        passing: stats.passing + 2,
+        dribbling: stats.dribbling + 2,
+        defending: stats.defending + 2,
+        physicality: stats.physicality + 2,
+      });
+    }
   };
 
   return [stats, increaseStats];
